965444694364be85de5fa688ce9530b11088675f
Adam Robson <adamray312@gmail.com>
2022-09-28 16:55:21 -0700

    redirect to signin if user not signed in

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { useItems } from '../../hooks/useItems';
 import { toggleListItem, createListItem } from '../../services/items';
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import { Redirect } from 'react-router-dom';
 
 export default function Items() {
   const [name, setName] = useState('');
@@ -8,7 +11,10 @@ export default function Items() {
 
   const { items, setItems } = useItems();
 
-  // TODO -- redirect the user back to auth if there is not a current user
+  const { user } = useContext(UserContext);
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
 
   const handleClick = async (item) => {
     try {
